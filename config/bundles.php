b7ee7493077b8552fbe289bcbd844d69588e8957
Marcin Malessa <marcin@malessa.pl>
2020-02-20 16:36:53 +0100

    Remove Broadway

--- config/bundles.php
+++ config/bundles.php
@@ -2,5 +2,4 @@
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Broadway\Bundle\BroadwayBundle\BroadwayBundle::class => ['all' => true],
 ];
--- src/Command/DoSomething.php
+++ src/Command/DoSomething.php
@@ -15,15 +15,15 @@ class DoSomething extends Command
     protected static $defaultName = 'app:do-something';
     private $commandBus;
 
-    public function __construct(CommandBus $commandBus)
-    {
-        $this->commandBus = $commandBus;
-        parent::__construct(null);
-    }
+//    public function __construct(CommandBus $commandBus)
+//    {
+//        $this->commandBus = $commandBus;
+//        parent::__construct(null);
+//    }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $sagaId = 'fdde7198-cdc8-4b9b-b5e1-7529dde2fed8';
-        $this->commandBus->dispatch(new RequestForAction($sagaId));
+//        $sagaId = 'fdde7198-cdc8-4b9b-b5e1-7529dde2fed8';
+//        $this->commandBus->dispatch(new RequestForAction($sagaId));
     }
 }
